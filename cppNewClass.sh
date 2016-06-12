8008d4c79e8c9c812e54ea6126675f71f39f4129
Jonas Knarbakk <jonas@knarbakk.no>
2016-06-12 10:00:33 +0200

    cppNewClass now adds to the makefile

--- cppNewClass.sh
+++ cppNewClass.sh
@@ -8,6 +8,10 @@ read cname
 touch $PWD/$cname.cpp
 touch $PWD/$cname.h
 echo "
-
 $cname.o: \$(SRC)$cname.cpp \$(SRC)$cname.h
 \t\$(CXX) \$(CXXFLAGS) \$(SRC)$cname.cpp" >> $PWD/../Makefile
+
+# Search for match and append filename to the end of the line
+sed -i "/output:\ \$(SRC)main.o/ s/$/ \$(SRC)$cname.o/" $PWD/../Makefile
+# Search for a match and append filename right after match
+sed -i "s/^\t\$(CXX)\ \$(SRC)main.o/&\ \$(SRC)$cname.o/" $PWD/../Makefile
